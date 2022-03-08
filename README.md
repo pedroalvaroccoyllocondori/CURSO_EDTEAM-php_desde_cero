3c666fe24c8db6aabe2e6d4c009d65a72839dffa
pedro alvaro <pedro_alvaro@outlook.com.pe>
2022-03-08 11:07:57 -0500

    insertando codigo php en html

---
+++ README.md
@@ -0,0 +1,11 @@
+iniciar un codigo en php
+<?php
+
+?>
+
+impresion de caracteres d
+
+\n--->salto de linea en las cadenas
+
+print--->no acepta la concatenacion por comas
+echo --->si acepta la concatenacion por comas
---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>hola a todos mis</h1>
+    
+</body>
+</html>
+
+
+
+
+
+
+
+
+<?php
+print "hola mundo";
+for ($i=1; $i < 5; $i++) { 
+    echo "<h1 style='color:blue; background:yellow'>hola a todos</h1>\n";
+}
+
+
+
+?>
+
+ 
