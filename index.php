a31afc620c7ef6f3204baef158952800e8c1e6b0
pedro alvaro <pedro_alvaro@outlook.com.pe>
2022-03-10 18:01:00 -0500

    variables y concatenacion y funcion para tipo de dato

--- index.php
+++ index.php
@@ -1,29 +1,37 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>hola a todos mis</h1>
-    
-    <?php
-    print "hola mundo";
-    for ($i=1; $i < 5; $i++) { 
-        echo "<h1 style='color:blue; background:yellow'>hola a todos</h1>\n";
-    }   
-    ?>
-    
-     
-</body>
-</html>
+<?php
 
+// variables en php
+$nombre="gato";
+$nombre=3.1416;
+$_nombre="gatito";
 
+echo $nombre , "\n";
+echo $_nombre , "\n";
 
+// concatenacion de variables con .
 
+$entero=100;
+$negativo=-63;
+$flotante=3.1416;
 
+echo $entero." ".$negativo." " .$flotante ,"\n";
 
+$octal=0755;
+$hexadecimal=0xC4E;
+$binario=0b1010;
 
+echo $octal." ".$hexadecimal." " .$binario ,"\n";
 
+//obtener el tipo de variables
+
+$divertido=true;
+
+echo "el tipo de dato es ". gettype($divertido);
+
+//obtener el tipo de dato con detalles 
+
+var_dump($divertido);
+
+?>
+
+ 
