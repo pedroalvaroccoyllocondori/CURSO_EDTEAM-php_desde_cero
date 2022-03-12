5e02a9e26c7eb300c2d61afa47f0a39a7d6a306d
pedro alvaro <pedro_alvaro@outlook.com.pe>
2022-03-12 18:44:45 -0500

    ESTRUCTURAS DE CONTROL DE FLUJO else, if, else if y operador ternario

--- index.php
+++ index.php
@@ -11,25 +11,30 @@
 // arreglos en php
 
 ?>
-<h1>mostrar y ocultar errores</h1>
+<h1>ESTRUCTUAS DE CONTROL else ,if ,else if</h1>
 <?php 
-#desactivar toda las notificaciones de error
-// error_reporting(0);
-$nombre='alvaro';
-echo $nombre;
-echo $nombre_completo;
 
-#guardar log de errores en
+// estructura  ELSE IF
 
-echo $noo;
-ini_set('log_errors', 1);
-ini_set('error_log','/tmp/php-error.log');
-error_log('hay un error')
+$numero=6;
+if ($numero==5) {
+    echo '<h2>el numero es igual a 5</h2>';
+}else if ($numero==6){
+    echo '<h2>el numero es igual a 6</h2>';
+}else if ($numero==7){
+    echo '<h2>el numero es igual a 7</h2>';
+}else{
+    echo '<h2>el numero no es igual a ninguno</h2>';
+}
 
+
+// OPERADOR TERNARIO remplaza  al if
+
+$A=($numero==2)?"es igual a 1":"no es igual a ninguno";
+echo "el resultado es ".'<span style="color:red">'.$A."<span/>"
 
 
 ?>
-   
     
 </body>
 </html>
