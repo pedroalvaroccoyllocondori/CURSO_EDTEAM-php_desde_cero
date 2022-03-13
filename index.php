2c36f7e4309aa8195c3e11f983f0395d7ee50784
pedro alvaro <pedro_alvaro@outlook.com.pe>
2022-03-13 16:06:45 -0500

    operador de triple igualdad('valor y tipo de dato')

--- index.php
+++ index.php
@@ -8,34 +8,18 @@
 </head>
 <body>
 <?php
-// arreglos en php
-
+// EJERCICIOS
 ?>
-<h1>ESTRUCTUAS DE CONTROL switch</h1>
+<h1>ejercicios</h1>
 <?php 
-
-// estructura  ELSE IF
-
-$numero=66;
-
-switch ($numero) {
-    case 2:
-        echo '<h2>el numero es igual a 2</h2>';
-        break;
-    
-    case 3:
-        echo '<h2>el numero es igual a 3</h2>';
-        break;
-    
-    case 6:
-        echo '<h2>el numero es igual a 6</h2>';
-        break;
-    
-    default:
-        echo '<h2>el numero es igual a ninguno</h2>';
-        break;
+$numero="100";
+if($numero === 100){
+    echo "es igual";
+}else{
+    echo "no es igual";
 }
 
+// operador and y or  USADO EN ESTRUCTURAS DE CONTROL DE FLUJO
 
 
 ?>
